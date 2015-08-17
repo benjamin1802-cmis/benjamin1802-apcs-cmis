a5552021acb505071909ebdb03fe24e60bf8329b
cmis <cmis@cmis-hs19.(none)>
2015-08-17 11:49:50 +0700

    added files

---
+++ HelloUsername/HelloUsername.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HelloUsername
+
+    {
+     public static void main( String[] args )
+        {
+           //System.out.println( "What is your username?" );
+           
+           Scanner input = new Scanner( System.in );
+           String statement = input.nextLine();
+           
+           System.out.println( statement );
+           
+        }// end method main
+        
+    } // end class Scanner
