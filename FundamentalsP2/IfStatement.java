530beed0baa2c85fb6e1deeec471a96532246d82
cmis <cmis@cmis-hs18.(none)>
2015-08-20 14:33:40 +0700

    added new stuff

---
+++ FundamentalsP2/IfStatement.java
@@ -0,0 +1,21 @@
+
+
+public class IfStatement{
+       public static void main ( String args[] )
+   {
+       int a = 1000;
+       // > greater than
+       // < less than
+       // ==equal to
+       // != not equal to
+       if( a > 500) // boolean statement
+        {
+            System.out.println( "The value of a is" );
+        }
+        else
+        { 
+            System.out.println("derp");
+        }
+    } // end method main
+    
+} // end class
--- FundamentalsPt1/Favorites.java
+++ FundamentalsPt1/Favorites.java
@@ -1,33 +1,21 @@
+import java.util.Scanner;
 
-/**
- * Write a description of class Favorites here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Favorites
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Favorites
-     */
-    public Favorites()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
-}
+        public static void main (String args[] )
+        {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Hello, your favorite food is Hamburger");
+            
+            System.out.println("Hello, your favorite movie is Rouge Nation.");
+            
+            System.out.println("Hello, your color is blue.");
+            
+            System.out.println("Hello, your musical group is Exo.");
+            
+            
+            
+             sc.close(); 
+  } // end main
+} // end class Scanner
+            
