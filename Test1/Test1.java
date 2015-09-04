e702a588b2016987fa0983e065aad276d7a0aaf8
cmis <cmis@cmis-hs18.(none)>
2015-09-04 11:17:20 +0700

    added .

---
+++ Test1/Test1.java
@@ -0,0 +1,11 @@
+import java.swing.JOptionPanel
+
+        public class Test1{
+            public static void main ( String args [])
+            
+            String one = JOptionPane.showInputDialog( "Insert a Number!" );
+            
+            
+            
+            
+            System.out.println( "O
