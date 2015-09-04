ef8212f658b27d716ff1c11f3fe104aba78b5935
cmis <cmis@cmis-hs18.(none)>
2015-09-04 11:54:04 +0700

    added .

--- Test1/Test1.java
+++ Test1/Test1.java
@@ -1,11 +1,57 @@
-import java.swing.JOptionPanel
+import java.swing.JOptionPanel;
 
         public class Test1{
-            public static void main ( String args [])
+            public static void main ( String args []);
+            {
+            String a = JOptionPane.showInputDialog( "Insert a Number!" );
+               
+            int numero1 = Integer.parseInt(a);
+            int numero2 = Interger.parseInt(b);
             
-            String one = JOptionPane.showInputDialog( "Insert a Number!" );
             
             
             
+           
+        for( int numero = 1;  numero <= 50 ; numero++ )
+            System.out.println( numero + "\t" + numero*numero + "\t" + ( int )(numero*Math.random()+numero));
+    } // end for
+                       
+            {
+            System.out.println( "5" );
+           
+            int[] array = new int[50] ;
+    
+            {
+                for( int index = 0; index < array.length; index++ )
+    
+            {
+        
+                array[index] =((int)(100*Math.random()));
+     
+            }//end for loop
+    
+            for( int index = 0; index < array.length; index++ )
+    
+            {
+        
+                System.out.println(array[index]);
+     
+            }//end for loop    
             
-            System.out.println( "O
+             {
+                 if( 
+                 {
+                   System.out.println( "E" );
+                    }// end if
+                    
+                  else if (
+                  {
+                        System.out.println("O")
+                     
+                 
+             {
+        } // end main
+
+            
+     
+               
