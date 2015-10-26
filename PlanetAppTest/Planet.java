80bf0c66ff601de89249b168e9cc1c864f2ec759
cmis <cmis@cmis-hs19.(none)>
2015-10-26 14:32:21 +0700

    added .

--- PlanetAppTest/Planet.java
+++ PlanetAppTest/Planet.java
@@ -4,32 +4,39 @@ public class Planet
     private double percentSpaceMarines;
     private String p;
     private double radius;
-    String[] namer= new String[]{"Beta" + "Alpha" + "Gama" + "Delta"};
+    String[] namer= new String[]{"beta" + "alpha" + "gama" + "Delta"};
 
     private String relationship;
+     
+    
+    public Planet()
+    {
+      relation = new String []{ "Hostile" , "At War", "Neutra", "Frenidly", "Ally"};
+    }
+    
     
     public Planet()
     {
         maxPopulation=1;
         maxSpaceMarinesPerStarDestroyer=1;
-        nplanets=1;
-        radius= 1.00;
-        percentSpaceMarines=1.0;
-        relationship= relationshiper[((int)Math.random())*5];
-        p = namer[((int)Math.random())*4] + String.format("%.2f", Math.random());
+        nPlanets=1;
+        Radius= 1;
+        PercentSpaceMarines=1;
+        Relationship= relationship[((int)Math.random())*5];
+        P = namer[((int)Math.random())*4] + String.format("%.2f", Math.random());
     }
     
     
     public String toString()
     {
-        String result = String.format( 
+        return String.format(
                 "Name: "+ p + "\n" +
                 "Relationship:" + relationship + "\n" +
                 "Radius:" + String.format("%.2f",radius) + "\n" +
                 "Population:" + String.format("%.2f", maxPopulation-((int)(Math.random())*maxPopulation)) + "\n" +
                 "Space Marines as percent of Pop.:" + percentSpaceMarines + "\n" +
-                "Space Maries:" + percentSpaceMarines*maxPopulation + "\n" +
-                "Star Destoyers:" + "" + "\n" +
+                "Space Marines:" + percentSpaceMarines*maxPopulation + "\n" +
+                "Star Destroyers:" + "" + "\n" +
                 "Space Marines per star destroyer:" + String.format("%.2f",maxSpaceMarinesPerStarDestroyer) + "\n");
         return result;
     }
@@ -41,13 +48,6 @@ public class Planet
         this.maxSpaceMarinesPerStarDestroyer = maxSpaceMarinesPerStarDestroyer;
     }
 
-
-
-    public Planet
-    {
-      relation = new String []{ "hostile" , "at war", "neutral", "friendly", "ally"};
-    }
-    
-    
+   
     
 }
