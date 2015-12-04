6c3140dc9e804aded20ee8e19758eed655f78606
cmis <cmis@cmis-hs16.(none)>
2015-12-04 11:16:52 +0700

    stuff erino

--- ClassDesignP3/Student.java
+++ ClassDesignP3/Student.java
@@ -1,39 +1,29 @@
 public class Student
 {
-    private int grade;
+    private int gradelevel;
+    private String firstname;
+    private String lastname;
     private double gpa;
-    private String firstName;
-    private String lastName;
-    private String Eden;
     public Student()
-        {
-            grade = 11;
-            gpa = 3.5;
-            firstName = "Ben";
-            lastName = "Chit";
-            Eden = "Derp \n" +
-                              "Nerp \n" +
-                              "Herpinonononononon";
-        }
-    
-    public Student(int grade, double gpa, String firstName, String lastName, String dBallZReference)
-        {
-            this.grade = grade;
-            this.gpa = gpa;
-            this.firstName = new String( firstName);
-            this.lastName = new String( lastName );
-            this.Eden = new String( Eden );
-        }
-    
-    public String toString()
-        {
-            String output = String.format("Student Info: " + "\n" +
-                                          "Name: " + firstName + " " + lastName + "\n" +
-                                          "Grade: " + grade + "\n" +
-                                          "GPA: " + gpa + "\n" +
-                                          Eden);
-            return output;
-        }
-        
-    
+    {
+        gradelevel =10;
+        firstname = new String ("Min");
+        lastname = new String ("Cho");
+        gpa = 2.60;
+    } //end constructor Song
+    public Student (String firstname, String lastname, int gradelevel, double gpa)
+    {
+        this.firstname = new String (firstname);
+        this.lastname = new String (lastname);
+        this.gradelevel = gradelevel;
+        this.gpa = gpa;
+    }
+
+    public String toString( )
+    {
+        String output = new String();
+        output = "'s full name is " + firstname +" " +lastname + " and has a gpa of "  + 
+        gpa + " studying at grade "+ gradelevel +"." ;
+        return output;
+    }
 }
--- ClassDesignP3/StudentDriver.java
+++ ClassDesignP3/StudentDriver.java
@@ -1,11 +1,11 @@
-public class StudentDriver
-{
-    public static void main ( String[] args )
+import javax.swing.JOptionPane;
+public class StudentDriver {
+    public static void main (String argvs [] )
     {
-       Student senior001 = new Student();
-       Student  junior001 = new Student(11, 5,5,"Deni", "Sane" , " ");
-       
-       System.out.println( senior001 );
-       System.out.println( junior001 );
+        Student senior001 = new Student ();
+        System.out.println ( "Senior001" +senior001);
+        Student junior001 = new Student ("Naruto", "Uzamaki", 10, 2.70);
+        System.out.println ( "Junior001" +junior001);
+        JOptionPane.showMessageDialog(null, "Senior001"+ senior001 +"  Junior001" +junior001, "Junior001 and Senior001", JOptionPane.INFORMATION_MESSAGE ) ;
     }
-}
+} //end class StudentDriver
